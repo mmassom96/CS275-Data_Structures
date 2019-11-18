ad7e4bc90f03023c612aa73a4c219b78b043bf35
Matthew Masom <m.massom96@gmail.com>
2019-11-18 00:17:44 -0600

    Homework 3

---
+++ CS275_HW3/src/Polynomial.java
@@ -0,0 +1,42 @@
+public class Polynomial {
+    public Polynomial(){
+        // polynomial of 0
+    }
+
+    public Polynomial(double a0){
+        // polynomial of a0
+    }
+
+    public Polynomial(Polynomial p){
+        // creates a copy of p
+    }
+
+    public void add_to_coef(double amount, int exponent){
+        // adds amount to the exponent term
+    }
+
+    public void assign_coef(double coefficient, int exponent){
+
+    }
+
+    public double coefficient(int exponent){
+
+    }
+
+    public double eval(double x){
+
+    }
+
+    public String toString(){
+
+    }
+
+    public Polynomial add(Polynomial p){
+
+    }
+
+    public Polynomial multiply(Polynomial p){
+
+    }
+
+}
---
+++ CS275_HW3/src/PolynomialTest.java
@@ -0,0 +1,93 @@
+public class PolynomialTest
+{
+  public static void main(String[] args)
+  {
+    Polynomial p1 = new Polynomial();
+    System.out.println("p1 = new Polynomial(): p1 = " + p1);
+    
+    p1.add_to_coef(1, 100);    
+    System.out.println("        p1.add_to_coef(1, 100), p1 = " + p1);
+    
+    p1.assign_coef(-1, 5);            
+    System.out.println("        p1.assign_coef(-1, 5),  p1 = " + p1);
+    
+    p1.add_to_coef(-1, 0);            
+    System.out.println("        p1.add_to_coef(-1, 0),  p1 = " + p1);
+    
+    System.out.println("        p1.coefficient(110) = " + p1.coefficient(110));
+    System.out.println("        p1.coefficient(80) = " + p1.coefficient(80));                       
+    System.out.println("        p1.coefficient(5) = " + p1.coefficient(5));                       
+    System.out.println("        p1.coefficient(3) = " + p1.coefficient(3));                       
+    
+                       
+    p1.assign_coef(0, 0);      
+    System.out.println("        p1.assign_coef(0, 0),  p1 = " + p1);
+    
+    p1.assign_coef(0, 100);            
+    System.out.println("        p1.assign_coef(0, 100),  p1 = " + p1);
+   
+    p1.add_to_coef(1, 0);
+    System.out.println("        p1.add_to_coef(1, 0),  p1 = " + p1);
+    
+    p1.assign_coef(0, 0);      
+    System.out.println("        p1.assign_coef(0, 0),  p1 = " + p1);
+    
+   System.out.println("        p1(2) = " + p1.eval(2)); 
+    System.out.println();
+    
+    Polynomial p3 = new Polynomial (p1);
+        
+    System.out.println("p3 = new Polynomial (p1),  p3 = " + p3); 
+    
+    System.out.println("        p3 == p1 " + (p3 == p1));
+    
+ //   System.out.println("        p3.equals(p1) " + (p3.equals(p1)));
+
+    p3.assign_coef(1, 5);           
+    System.out.println("        p3.assign_coef(1, 5),  p3 = " + p3);
+  
+   // System.out.println("        p3.equals(p1) " + (p3.equals(p1)));
+        
+    p3.assign_coef(2,2);
+    
+    System.out.println("        p3.assign_coef(2,2),  p3 = " + p3);
+    
+ 
+    System.out.println("        p3(2) = " + p3.eval(2)); 
+    System.out.println();
+   
+    
+    p1.assign_coef(-2,2);
+     System.out.println("        p1.assign_coef(-2,2),  p1 = " + p1);   
+    
+    System.out.println("        p3 + p1= " + p3.add(p1));   
+  
+    
+    System.out.println();      
+    Polynomial p2 = new Polynomial(1); 
+    
+    System.out.println("p2 = new Polynomial(1): p2 = " + p2); 
+    
+    p2.assign_coef(1, 5);   
+    
+    System.out.println("      p2.assign_coef(1,5), p2 = " + p2);
+    System.out.println();  
+    System.out.println("      p2 * (-1) = " + p2.multiply(new Polynomial(-1)));  
+    
+    
+    System.out.println("      p2 * p1 = " + p1.multiply(p2));    
+    
+    p1.assign_coef(0,2);
+    System.out.println("      p1.assign_coef(0,2),  p1 = " + p1); 
+    
+    p1.assign_coef(-1,0);
+    System.out.println("      p1.assign_coef(-1,0),  p1 = " + p1); 
+    
+    p1.assign_coef(1,5);
+    System.out.println("      p1.assign_coef(1,5),  p1 = " + p1); 
+    
+    System.out.println("      p2 * p1 = " + p1.multiply(p2));    
+    
+ 
+  }
+}
